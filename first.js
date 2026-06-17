0b83758eb6a3b096cf3a4cdea961fd036d33ec40
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-17 11:30:02 +0530

    created delete btn for tasks indivisually

--- first.js
+++ first.js
@@ -1,11 +1,28 @@
 let search_task = document.getElementById("src-task");
-
 let new_task = document.getElementById("new-task");
-
 let task_list = document.getElementById("task-list");
-
 let add_btn = document.getElementById("add-task-btn");
 
-add_btn.addEventListener("click" , function(click){
-    console.log(new_task.value);
+add_btn.addEventListener("click", function () {
+    let taskText = new_task.value;
+
+    let li = document.createElement("li");
+
+    let deletebtn = document.createElement("button");
+
+    li.innerText = taskText;
+
+    task_list.appendChild(li);
+
+    new_task.value = "";
+
+    deletebtn.innerText = "delete";    
+
+    li.appendChild(deletebtn);
+
+    deletebtn.addEventListener("click" , function () {
+        li.remove();
+        
+    })
+
 });
